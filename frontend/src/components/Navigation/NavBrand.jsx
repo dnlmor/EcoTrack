2d39b08d9a18d6370f773eb5a4ae9fd8a0c2a9cc
danielsiahaan <morenosiahaan07@gmail.com>
2025-01-27 16:40:18 +0100

    modify backend workspace

--- frontend/src/components/Navigation/NavBrand.jsx
+++
@@ -1,8 +0,0 @@
-const NavBrand = () => (
-    <Link to="/" className="flex items-center space-x-2">
-      <span className="text-2xl">🌱</span>
-      <span className="text-xl font-bold">EcoTrack</span>
-    </Link>
-   );
-
-export default NavBrand;
